f03501c0463e221dfaa5a77f99753a90e8dd8988
Leon Dick <ldick@1-D-32.42heilbronn.de>
2024-05-25 06:13:36 +0200

    milk

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 07:31:59 by ldick             #+#    #+#             */
-/*   Updated: 2024/05/24 08:23:23 by ldick            ###   ########.fr       */
+/*   Updated: 2024/05/25 06:12:52 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	main(int argc, char *argv[])
 	int		o;
 	int		i;
 	int		k;
+	char	**milk;
 
+	milk = NULL;
 	k = 1;
 	o = 0;
 	i = 0;
@@ -27,6 +29,27 @@ int	main(int argc, char *argv[])
 		k++;
 		i++;
 	}
-	printf("%d\n", o);
+	i = 0;
+	k = 0;
+	while (i < argc)
+	{
+		milk = ft_split(&argv[k][i], ' ');
+		if (!argv && i + 1 < argc)
+			k++;
+		i++;
+	}
+	k = 0;
+	i = 0;
+	while (k < o)
+	{
+		printf("%c", milk[i][k]);
+		if (milk[i + 1] == (void *)0)
+		{
+			k++;
+			i = 0;
+			printf("\n");
+		}
+		i++;
+	}
 	return (0);
 }
